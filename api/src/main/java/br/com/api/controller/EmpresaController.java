6b214b659b73433cc3bb8745ebe8537597e9be56
leafarpontes <rafapontes2008@hotmail.com>
2022-04-18 22:13:45 -0300

    Concluido: Cadastro, Atualização e Listagem de Funcionários

--- api/src/main/java/br/com/api/controller/EmpresaController.java
+++ api/src/main/java/br/com/api/controller/EmpresaController.java
@@ -1,16 +1,17 @@
 package br.com.api.controller;
 
 import br.com.api.models.Empresa;
+import br.com.api.models.Funcionario;
 import br.com.api.models.Holerite;
 import br.com.api.repository.EmpresaRepository;
+import br.com.api.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,6 +20,9 @@ public class EmpresaController {
 
     @Autowired
     private EmpresaRepository empresaRepository;
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
 
     @GetMapping("/{idEmpresa}")
     public ResponseEntity<Empresa> buscarEmpresa(@PathVariable Long idEmpresa){
@@ -31,4 +35,66 @@ public class EmpresaController {
 
         return ResponseEntity.status(HttpStatus.OK).body(empresaOptional.get());
     }
+
+    @PostMapping("/cadastrar-funcionario")
+    @Transactional
+    public ResponseEntity<Funcionario> cadastrarFuncionario(@RequestBody Funcionario funcionario) {
+
+        Optional<Empresa> empresa = empresaRepository.findById(1l);
+
+        empresa.ifPresent(funcionario::setEmpresa);
+
+        Funcionario funcionarioSalvo = funcionarioRepository.save(funcionario);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(funcionarioSalvo);
+    }
+
+    @GetMapping("/funcionarios")
+    public ResponseEntity<List<Funcionario>> listarFuncionario() {
+
+        List<Funcionario> funcionarios = funcionarioRepository.findAll();
+
+        return ResponseEntity.status(HttpStatus.OK).body(funcionarios);
+    }
+
+    @GetMapping("funcionario/{idFuncionario}")
+    public ResponseEntity<Funcionario> buscarFuncionario(@PathVariable Long idFuncionario) {
+
+        Optional<Funcionario> funcionarioOptional = funcionarioRepository.findById(idFuncionario);
+
+        if (funcionarioOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(funcionarioOptional.get());
+    }
+
+    @PutMapping("/atualizar-funcionario/{idFuncionario}")
+    @Transactional
+    public ResponseEntity<Funcionario> atualizarFuncionario(@RequestBody Funcionario funcionario,
+                                                            @PathVariable Long idFuncionario) {
+
+        Optional<Funcionario> funcionarioBuscado = funcionarioRepository.findById(idFuncionario);
+
+        if (funcionarioBuscado.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Funcionario funcionario1 = funcionarioBuscado.get();
+
+        funcionario1.setNome(funcionario.getNome());
+        funcionario1.setSalario(funcionario.getSalario());
+        funcionario1.setFuncao(funcionario.getFuncao());
+        funcionario1.setAdicionalPericulosidade(funcionario.getAdicionalPericulosidade());
+        funcionario1.setJornadaDeTrabalho(funcionario.getJornadaDeTrabalho());
+        funcionario1.setHorasExtras(funcionario.getHorasExtras());
+        funcionario1.setDependentes(funcionario.getDependentes());
+        funcionario1.setAjudaDeCusto(funcionario.getAjudaDeCusto());
+        funcionario1.setValorPlanoDeSaude(funcionario.getValorPlanoDeSaude());
+
+        Funcionario funcionarioSalvo = funcionarioRepository.save(funcionario1);
+
+        return ResponseEntity.status(HttpStatus.OK).body(funcionarioSalvo);
+    }
+
 }
--- api/src/main/resources/import.sql
+++ api/src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO empresas (cnpj, razao_social) VALUES ("123456789","XP_INC")
 INSERT INTO funcionarios (adicional_periculosidade, ajuda_de_custo, dependentes, funcao, horas_extras, jornada_de_trabalho, nome, salario, valor_plano_de_saude) VALUES (30.00, 300.00, 2, "Desenvolvedor Jr.", 10, 176, "Jerry", 3500.00, 50.00)
 UPDATE funcionarios SET empresa_id = 1 WHERE id = 1
+INSERT INTO funcionarios (adicional_periculosidade, ajuda_de_custo, dependentes, funcao, horas_extras, jornada_de_trabalho, nome, salario, valor_plano_de_saude, empresa_id) VALUES (30.00, 300.00, 0, "Desenvolvedor Pleno", 20, 176, "Kramer", 6500.00, 50.00, 1)
