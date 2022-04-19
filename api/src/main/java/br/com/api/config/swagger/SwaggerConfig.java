0ac43ae7a5fcf595d3342090dff8116d5ee95d76
Lucas Trevizan <72326473+LucasTrevizanbr@users.noreply.github.com>
2022-04-19 16:54:20 -0300

    Adicionando o swagger

---
+++ api/src/main/java/br/com/api/config/swagger/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package br.com.api.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.api"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("API Poseidon")
+                .description("Parte de um processo de folha de pagamento, com consultas e geração da folha.")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact(){
+        return new Contact("SSCM", "https://github.com/SSCM-Squad", "sscm@email.com");
+    }
+}
