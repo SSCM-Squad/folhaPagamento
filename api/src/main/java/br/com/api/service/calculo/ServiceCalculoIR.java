95ac914c9146c2c027177faa3b39fc453ffe8dc6
TML45 <tassio.melo45@gmail.com>
2022-04-23 21:23:45 -0300

    Finalizada geração de holerite

--- api/src/main/java/br/com/api/service/calculo/ServiceCalculoIR.java
+++ api/src/main/java/br/com/api/service/calculo/ServiceCalculoIR.java
@@ -1,30 +1,50 @@
 package br.com.api.service.calculo;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+
 
 public class ServiceCalculoIR {
-
+	
+	private BigDecimal salarioBaseIR;	
+	
 	BigDecimal valorDependente = new BigDecimal("189.59");
     private final BigDecimal[] aliquota = {new BigDecimal("0.075"), new BigDecimal("0.15"), new BigDecimal("0.225"), new BigDecimal("0.275") };
     private final BigDecimal[] deducao = {new BigDecimal("142.80") ,new BigDecimal("354.80") ,new BigDecimal("636.13") ,new BigDecimal("869.36")  };
 
-    BigDecimal calculoIR(BigDecimal salarioMenosINSS, BigDecimal qtdDependentes, BigDecimal outrosDescontos, BigDecimal pensaoAlimenticia,
+    public HashMap<String,BigDecimal> calculoIR(BigDecimal totalVtMenosINSS, int qtdDependentes, BigDecimal outrosDescontos, BigDecimal pensaoAlimenticia,
     		BigDecimal previdenciaSocial) {
-
-        BigDecimal salarioBaseIR = salarioMenosINSS.subtract(valorDependente.multiply(qtdDependentes) ).subtract(outrosDescontos).subtract(pensaoAlimenticia).subtract(previdenciaSocial);
+    	HashMap<String,BigDecimal> valorEAliquota = new HashMap<>();
+    	
+        this.salarioBaseIR = totalVtMenosINSS.subtract(valorDependente.multiply(new BigDecimal(qtdDependentes)) ).subtract(outrosDescontos).subtract(pensaoAlimenticia).subtract(previdenciaSocial);
 
         if (salarioBaseIR.compareTo(new BigDecimal("1900")) <= 0) {
-            return BigDecimal.ZERO;
+        	valorEAliquota.put("Isento", BigDecimal.ZERO);
+            return valorEAliquota;
+            
         } else if (salarioBaseIR.compareTo(new BigDecimal("1900.01")) >= 0 && salarioBaseIR.compareTo(new BigDecimal("2800.00")) <= 0) {
-        	return salarioBaseIR.multiply(aliquota[0]).subtract(deducao[0]);
+        	valorEAliquota.put(aliquota[0].toString(), salarioBaseIR.multiply(aliquota[0]).subtract(deducao[0]));
+        	return valorEAliquota;
+        	
         } else if (salarioBaseIR.compareTo(new BigDecimal("2800.01")) >= 0 && salarioBaseIR.compareTo(new BigDecimal("3751.00")) <= 0) {
-        	return salarioBaseIR.multiply(aliquota[1]).subtract(deducao[1]);
+        	valorEAliquota.put(aliquota[1].toString(), salarioBaseIR.multiply(aliquota[1]).subtract(deducao[1]));
+        	return valorEAliquota;
+        	
         } else if (salarioBaseIR.compareTo(new BigDecimal("3751.01")) >= 0 && salarioBaseIR.compareTo(new BigDecimal("4664.00")) <= 0) {
-            return salarioBaseIR.multiply(aliquota[2]).subtract(deducao[2]);
+        	valorEAliquota.put(aliquota[2].toString(), salarioBaseIR.multiply(aliquota[2]).subtract(deducao[2]));
+        	return valorEAliquota;
+            
         } else {
-        	return salarioBaseIR.multiply(aliquota[3]).subtract(deducao[3]);
+        	valorEAliquota.put(aliquota[3].toString(), salarioBaseIR.multiply(aliquota[3]).subtract(deducao[3]));
+        	return valorEAliquota;
         }
     }
+
+	public BigDecimal getSalarioBaseIR() {
+		return salarioBaseIR;
+	}
+    
+    
 
 
 }
--- api/src/main/java/br/com/api/service/holerite/ServiceHolerite.java
+++ api/src/main/java/br/com/api/service/holerite/ServiceHolerite.java
@@ -4,88 +4,118 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.api.models.Detalhe;
 import br.com.api.models.Funcionario;
 import br.com.api.models.Holerite;
+import br.com.api.repository.HoleriteRepository;
 import br.com.api.service.calculo.ServiceCalculoFGTS;
 import br.com.api.service.calculo.ServiceCalculoINSS;
+import br.com.api.service.calculo.ServiceCalculoIR;
 
 @Service
 public class ServiceHolerite {
+	
+	@Autowired
+	private HoleriteRepository holeriteRepository;
 
 	public Holerite gerarHolerite(Funcionario funcionario, LocalDate data) {
 		Holerite holerite = new Holerite();
-		
+
 		holerite.getCabecalho().setData(data);
 		holerite.getCabecalho().setCargoFuncionario(funcionario.getFuncao());
 		holerite.getCabecalho().setCnpjEmpresa(funcionario.getEmpresa().getCnpj());
 		holerite.getCabecalho().setCod("COD");
 		holerite.getCabecalho().setNomeEmpresa(funcionario.getEmpresa().getRazaoSocial());
 		holerite.getCabecalho().setNomeFuncionario(funcionario.getNome());
-		
-		
+
 		Detalhe detalheSalario = new Detalhe();
 		detalheSalario.setDescricao("Salário");
 		detalheSalario.setVencimento(funcionario.getSalario());
 		detalheSalario.setReferencia("30 dias");
-		
+
 		Detalhe detalheAjudaDeCusto = new Detalhe();
 		detalheAjudaDeCusto.setDescricao("Ajuda de Custo");
 		detalheAjudaDeCusto.setVencimento(funcionario.getAjudaDeCusto());
 		detalheAjudaDeCusto.setReferencia("-");
-		
+
 		Detalhe detalhePericulosidade = new Detalhe();
-		BigDecimal valorVencimentoPericulosidade = funcionario.getSalario().multiply(funcionario.getAdicionalPericulosidade());
+		BigDecimal valorVencimentoPericulosidade = funcionario.getSalario()
+				.multiply(funcionario.getAdicionalPericulosidade());
 		detalhePericulosidade.setDescricao("Periculosidade");
 		detalhePericulosidade.setVencimento(valorVencimentoPericulosidade);
 		detalhePericulosidade.setReferencia("30%");
-		
+
 		Detalhe detalheHorasExtraordinarias = new Detalhe();
 		BigDecimal valorDaHora = funcionario.getSalario().divide(new BigDecimal(funcionario.getJornadaDeTrabalho()));
-		BigDecimal vencimentoHoraExtra = valorDaHora.multiply(new BigDecimal(funcionario.getHorasExtras())).multiply(new BigDecimal("1.5"));
+		BigDecimal vencimentoHoraExtra = valorDaHora.multiply(new BigDecimal(funcionario.getHorasExtras()))
+				.multiply(new BigDecimal("1.5"));
 		detalheHorasExtraordinarias.setDescricao("Horas Extra Ordinárias");
-		detalheHorasExtraordinarias.setVencimento(vencimentoHoraExtra.setScale(2,RoundingMode.HALF_UP));
+		detalheHorasExtraordinarias.setVencimento(vencimentoHoraExtra.setScale(2, RoundingMode.HALF_UP));
 		detalheHorasExtraordinarias.setReferencia(String.valueOf(funcionario.getHorasExtras()));
-		
+
 		Detalhe detalhePlanoDeSaude = new Detalhe();
 		detalhePlanoDeSaude.setDescricao("Plano de Saúde");
 		detalhePlanoDeSaude.setDesconto(funcionario.getValorPlanoDeSaude());
 		detalhePlanoDeSaude.setReferencia("-");
-		
+
 		List<Detalhe> detalhes = new ArrayList<>();
 		detalhes.add(detalheSalario);
 		detalhes.add(detalheAjudaDeCusto);
 		detalhes.add(detalhePericulosidade);
 		detalhes.add(detalheHorasExtraordinarias);
 		detalhes.add(detalhePlanoDeSaude);
-		
-		
+
 		holerite.getRodape().setDependentes(funcionario.getDependentes());
 		holerite.getRodape().setSalarioBase(funcionario.getSalario());
-		BigDecimal totalVencimentos = detalhes.stream().map(detalhe -> detalhe.getVencimento()).reduce(BigDecimal::add).get();
+		BigDecimal totalVencimentos = detalhes.stream().map(detalhe -> detalhe.getVencimento()).reduce(BigDecimal::add)
+				.get();
 		holerite.getRodape().setTotalVencimentos(totalVencimentos);
 		BigDecimal totalVencimentosTributaveis = totalVencimentos.subtract(funcionario.getAjudaDeCusto());
-		holerite.getRodape().setTotalDescontos(detalhes.stream().map(detalhe -> detalhe.getDesconto()).reduce(BigDecimal::add).get());
-		holerite.getRodape().setValorFgts(ServiceCalculoFGTS.calcularContribuicaoFGTS(totalVencimentosTributaveis, funcionario.getTipo()));
-		
+		holerite.getRodape().setBaseInss(totalVencimentosTributaveis);
+		holerite.getRodape().setValorFgts(
+				ServiceCalculoFGTS.calcularContribuicaoFGTS(totalVencimentosTributaveis, funcionario.getTipo()));
+		holerite.getRodape().setValorLiquido(totalVencimentos);
+
 		Detalhe detalheINSS = new Detalhe();
 		detalheINSS.setDescricao("INSS");
 		ServiceCalculoINSS calculoINSS = new ServiceCalculoINSS();
 		BigDecimal valorINSS = calculoINSS.calcularContribuicaoINSS(totalVencimentosTributaveis);
 		detalheINSS.setDesconto(valorINSS);
-		detalheINSS.setReferencia(valorINSS.divide(totalVencimentosTributaveis).multiply(new BigDecimal("100")) .toString());
+		detalheINSS.setReferencia(
+				valorINSS.divide(totalVencimentosTributaveis).multiply(new BigDecimal("100")).toString());
 		detalhes.add(detalheINSS);
-		
+
+		BigDecimal totalVtMenosINSS = totalVencimentosTributaveis.subtract(valorINSS);
+		ServiceCalculoIR calculoIR = new ServiceCalculoIR();
+
 		Detalhe detalheIR = new Detalhe();
 		detalheIR.setDescricao("IRRF");
-		detalheIR.setDesconto(valorINSS);
-		
+		HashMap<String, BigDecimal> valorEAliquota = calculoIR.calculoIR(totalVtMenosINSS, funcionario.getDependentes(),
+				BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO);
+		detalheIR.setDesconto(valorEAliquota.entrySet().stream().map(Map.Entry::getValue).findFirst().get());
+		detalheIR.setReferencia(valorEAliquota.entrySet().stream().map(Map.Entry::getKey).findFirst().get());
+		detalhes.add(detalheIR);
+
+		BigDecimal totalDescontos = detalhes.stream().map(detalhe -> detalhe.getDesconto()).reduce(BigDecimal::add)
+				.get();
+		holerite.getRodape().setTotalDescontos(totalDescontos);
+
+		holerite.getRodape().setBaseIrpf(calculoIR.getSalarioBaseIR());
+
+		holerite.getRodape().setValorLiquido(totalVencimentos.subtract(totalDescontos));
 		holerite.setDetalhes(detalhes);
-		return null;
+
+		holerite.setFuncionario(funcionario);
+		
+
+		return holeriteRepository.save(holerite);
 	}
 
 }
