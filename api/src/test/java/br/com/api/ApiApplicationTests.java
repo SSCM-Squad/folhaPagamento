7d8b23a0a5a65e975aaf7ce0a97a884fe82371ab
Lucas Trevizan <72326473+LucasTrevizanbr@users.noreply.github.com>
2022-04-28 16:35:51 -0300

    Testando a criação do jar por linha de comando para colocar no readme

--- api/src/test/java/br/com/api/ApiApplicationTests.java
+++ api/src/test/java/br/com/api/ApiApplicationTests.java
@@ -1,13 +1,29 @@
 package br.com.api;
 
+import br.com.api.controller.EmpresaController;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private EmpresaController empresaController;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(empresaController).isNotNull();
+    }
+
+
 
 }
